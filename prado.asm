66a06abb8dcb18ae3e0b6a117b0c41f8aecc8cda
jabberdrake <jabberdrake@gmail.com>
2022-06-02 15:53:35 +0100

    display a semi funcionar

--- prado.asm
+++ prado.asm
@@ -1,6 +1,6 @@
 ; *********************************************************************************
 ; * IST-UL
-; * Modulo:    lab5-move-boneco-teclado-toque.asm
+; * Modulo:    prado.asm
 ; * Descrição: Este programa ilustra o movimento de um boneco do ecrã, sob controlo
 ; *			do teclado, em que o boneco só se movimenta um pixel por cada
 ; *			tecla carregada (produzindo também um efeito sonoro).
@@ -13,8 +13,11 @@ TEC_LIN					EQU 0C000H		; endereço das linhas do teclado (periférico POUT-2)
 TEC_COL					EQU 0E000H		; endereço das colunas do teclado (periférico PIN)
 LINHA_TECLADO			EQU 8			; linha a testar (4ª linha, 1000b)
 MASCARA					EQU 0FH			; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-TECLA_ESQUERDA			EQU 0H			; tecla na primeira coluna do teclado (tecla C)
-TECLA_DIREITA			EQU 2H			; tecla na segunda coluna do teclado (tecla D)
+TECLA_ESQUERDA			EQU 0			; tecla 0
+TECLA_DIREITA			EQU 2			; tecla 2
+TECLA_METEORO_BAIXO		EQU 7			; tecla 7
+TECLA_AUMENTA_DISPLAY 	EQU 13 			; tecla D
+TECLA_DIMINUI_DISPLAY	EQU 12			; tecla C
 
 DEFINE_LINHA    		EQU 600AH      ; endereço do comando para definir a linha
 DEFINE_COLUNA   		EQU 600CH      ; endereço do comando para definir a coluna
@@ -34,6 +37,11 @@ ATRASO			EQU	0FFFH			   ; atraso para limitar a velocidade de movimento do bonec
 LARGURA			EQU	5				   ; largura do boneco
 COR_PIXEL		EQU	0FF00H			   ; cor do pixel: vermelho em ARGB (opaco e vermelho no máximo, verde e azul a 0)
 
+ENDEREÇO_DISPLAY EQU 0A000H			   ; endereço do display (POUT-2)
+ENERGIA_INICIAL	EQU	100				   ; energia inicial do boneco
+VALOR_ENERGIA_AUMENTO EQU 5			   ; valor de aumento da energia
+VALOR_ENERGIA_DIMINUI EQU 5			   ; valor de diminuição da energia
+
 ; *********************************************************************************
 ; * Dados 
 ; *********************************************************************************
@@ -44,6 +52,9 @@ pilha:
 SP_inicial:				; este é o endereço (1200H) com que o SP deve ser 
 						; inicializado. O 1.º end. de retorno será 
 						; armazenado em 11FEH (1200H-2)
+
+vida:
+	WORD ENERGIA_INICIAL 	; guarda a energia inicial do boneco
 							
 DEF_BONECO:					; tabela que define o boneco (cor, largura, pixels)
 	WORD		LARGURA
@@ -64,19 +75,20 @@ inicio:
     MOV  [SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
 	MOV	 R7, 1							; valor a somar à coluna do boneco, para o movimentar
 	MOV  R6, LINHA_TECLADO				; inicializa R6 com o valor da primeira linha a ser lida
+	CALL inicia_energia_display
      
 posição_boneco:
     MOV  R1, LINHA						; linha do boneco
     MOV  R2, COLUNA						; coluna do boneco
 	MOV	 R4, DEF_BONECO					; endereço da tabela que define o boneco
 
-mostra_boneco:
-	CALL desenha_boneco					; desenha o boneco a partir da tabela
-
 espera_nao_tecla:						; neste ciclo espera-se até NÃO haver nenhuma tecla premida
 	CALL teclado						; leitura às teclas dado a linha (R6) anteriormente gravada
 	CMP	 R0, -1							; se R0 = -1, nenhuma tecla está a ser premida
 	JNZ	 espera_nao_tecla				; espera, enquanto houver tecla uma tecla carregada
+
+mostra_boneco:
+	CALL desenha_boneco					; desenha o boneco a partir da tabela
 
 espera_tecla:				; neste ciclo espera-se até uma tecla ser premida
 	ROL R6, 1				; linha a testar no teclado
@@ -86,15 +98,33 @@ espera_tecla:				; neste ciclo espera-se até uma tecla ser premida
 	
 	MOV	R9, 0			    ; som com número 0
 	MOV [TOCA_SOM], R9		; comando para tocar o som
-	
+mover_esquerda:
 	CMP	R0, TECLA_ESQUERDA
-	JNZ	testa_direita
-	MOV	R7, -1			; vai deslocar para a esquerda
-	JMP	ve_limites
-testa_direita:
-	CMP	R0, TECLA_DIREITA
-	JNZ	espera_tecla		; tecla que não interessa
-	MOV	R7, +1			; vai deslocar para a direita
+	JNZ mover_direita
+	MOV R7, -1 				; vai deslocar para a esquerda
+	JMP ve_limites
+mover_direita:
+	CMP R0, TECLA_DIREITA
+	JNZ aumenta_display
+	MOV R7, +1 				; vai deslocar para a direita
+	JMP ve_limites
+;baixar_meteoro:
+;	CMP R0, TECLA_METEORO_BAIXO
+;	JNZ aumenta_display
+;	CALL posicao_meteoro_mau
+;	JMP espera_nao_tecla
+aumenta_display:
+	MOV R3, TECLA_AUMENTA_DISPLAY
+	CMP R0, R3
+	JNZ diminui_display
+	CALL aumenta_energia_display
+	JMP espera_nao_tecla
+diminui_display:
+	MOV R3, TECLA_DIMINUI_DISPLAY
+	CMP R0, R3
+	JNZ espera_tecla
+	CALL diminui_energia_display
+	JMP espera_nao_tecla
 	
 ve_limites:
 	PUSH R6
@@ -132,9 +162,9 @@ desenha_pixels:       		; desenha os pixels do boneco a partir da tabela
 	MOV	R3, [R4]			; obtém a cor do próximo pixel do boneco
 	CALL	escreve_pixel		; escreve cada pixel do boneco
 	ADD	R4, 2			; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
-     ADD  R2, 1               ; próxima coluna
-     SUB  R5, 1			; menos uma coluna para tratar
-     JNZ  desenha_pixels      ; continua até percorrer toda a largura do objeto
+    ADD  R2, 1               ; próxima coluna
+    SUB  R5, 1			; menos uma coluna para tratar
+    JNZ  desenha_pixels      ; continua até percorrer toda a largura do objeto
 	POP	R5
 	POP	R4
 	POP	R3
@@ -160,9 +190,9 @@ apaga_pixels:       		; desenha os pixels do boneco a partir da tabela
 	MOV	R3, 0			; cor para apagar o próximo pixel do boneco
 	CALL	escreve_pixel		; escreve cada pixel do boneco
 	ADD	R4, 2			; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
-     ADD  R2, 1               ; próxima coluna
-     SUB  R5, 1			; menos uma coluna para tratar
-     JNZ  apaga_pixels      ; continua até percorrer toda a largura do objeto
+    ADD  R2, 1               ; próxima coluna
+    SUB  R5, 1			; menos uma coluna para tratar
+    JNZ  apaga_pixels      ; continua até percorrer toda a largura do objeto
 	POP	R5
 	POP	R4
 	POP	R3
@@ -170,13 +200,13 @@ apaga_pixels:       		; desenha os pixels do boneco a partir da tabela
 	RET
 
 
-; **********************************************************************
-; ESCREVE_PIXEL - Escreve um pixel na linha e coluna indicadas.
-; Argumentos:   R1 - linha
-;               R2 - coluna
-;               R3 - cor do pixel (em formato ARGB de 16 bits)
-;
-; **********************************************************************
+; *
+; * ESCREVE_PIXEL - Escreve um pixel na linha e coluna indicadas.
+; * Argumentos:   R1 - linha
+; *               R2 - coluna
+; *               R3 - cor do pixel (em formato ARGB de 16 bits)
+; *
+; *
 escreve_pixel:
 	MOV  [DEFINE_LINHA], R1		; seleciona a linha
 	MOV  [DEFINE_COLUNA], R2		; seleciona a coluna
@@ -184,11 +214,11 @@ escreve_pixel:
 	RET
 
 
-; **********************************************************************
-; ATRASO - Executa um ciclo para implementar um atraso.
-; Argumentos:   R11 - valor que define o atraso
-;
-; **********************************************************************
+; *
+; * ATRASO - Executa um ciclo para implementar um atraso.
+; * Argumentos:   R11 - valor que define o atraso
+; *
+; *
 atraso:
 	PUSH	R11
 ciclo_atraso:
@@ -197,16 +227,16 @@ ciclo_atraso:
 	POP	R11
 	RET
 
-; **********************************************************************
-; TESTA_LIMITES - Testa se o boneco chegou aos limites do ecrã e nesse caso
-;			   impede o movimento (força R7 a 0)
-; Argumentos:	R2 - coluna em que o objeto está
-;			R6 - largura do boneco
-;			R7 - sentido de movimento do boneco (valor a somar à coluna
-;				em cada movimento: +1 para a direita, -1 para a esquerda)
-;
-; Retorna: 	R7 - 0 se já tiver chegado ao limite, inalterado caso contrário	
-; **********************************************************************
+; *
+; * TESTA_LIMITES - Testa se o boneco chegou aos limites do ecrã e nesse caso
+; *			   impede o movimento (força R7 a 0)
+; * Argumentos:	R2 - coluna em que o objeto está
+; * 			R6 - largura do boneco
+; * 			R7 - sentido de movimento do boneco (valor a somar à coluna
+; *				em cada movimento: +1 para a direita, -1 para a esquerda)
+; *
+; * Retorna: 	R7 - 0 se já tiver chegado ao limite, inalterado caso contrário	
+; *
 testa_limites:
 	PUSH	R5
 	PUSH	R6
@@ -232,13 +262,13 @@ sai_testa_limites:
 	POP	R5
 	RET
 
-; +----------------------------------------------------------------------------------------------+
-; | TECLADO - Faz uma leitura às teclas de uma linha do teclado e retorna o valor da tecla lida  |
-; | Argumentos:	R6 - linha a testar (em formato 1, 2, 4 ou 8)                                    |
-; |                                                                                              |
-; | Retorna: 	R0 - valor (em hexadecimal) da tecla premida (em formato 0, 1, 2, ..., F)        |
-; |				NOTA: Caso nenhuma tecla esteja premida, R0 fica a -1.                           |	
-; +----------------------------------------------------------------------------------------------+
+; *
+; * TECLADO - Faz uma leitura às teclas de uma linha do teclado e retorna o valor da tecla lida
+; * Argumentos:	R6 - linha a testar (em formato 1, 2, 4 ou 8)
+; *
+; * Retorna: 	R0 - valor (em hexadecimal) da tecla premida (em formato 0, 1, 2, ..., F)
+; *				NOTA: Caso nenhuma tecla esteja premida, R0 fica a -1.	
+; *
 teclado:
 	PUSH	R2
 	PUSH	R3
@@ -285,4 +315,29 @@ formata_coluna_ciclo:
 	CMP R0, 0
 	JNZ formata_coluna_ciclo
 	MOV R0, TEMP
+	RET
+
+inicia_energia_display:
+	PUSH R0							;guarda o valor de R0
+	MOV R0, [vida]					;coloca em R0 o valor inicial da energia
+	MOV [ENDEREÇO_DISPLAY], R0		;coloca o valor inicial no display
+	POP R0
+	RET
+
+aumenta_energia_display:
+	PUSH R0							;guarda o valor de R0
+	MOV R0, [vida]					;coloca em R0 o valor inicial da energia
+	ADD R0, 5
+	MOV [vida], R0					;Guarda energia na memória
+	MOV [ENDEREÇO_DISPLAY], R0		;coloca o valor inicial no display
+	POP R0							;restaura o valor de R0
+	RET
+
+diminui_energia_display:
+	PUSH R0							;guarda o valor de R0
+	MOV R0, [vida]					;coloca em R0 o valor inicial da energia
+	SUB R0, 5
+	MOV [vida], R0					;Guarda energia na memória
+	MOV [ENDEREÇO_DISPLAY], R0		;coloca o valor inicial no display
+	POP R0							;restaura o valor de R0
 	RET
